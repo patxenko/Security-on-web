eb882d0f6cc3fd2b885937a7fac2a037a8bdb9a6
4RL3K1N <fran4_lofer@hotmail.com>
2016-10-18 10:41:36 +0200

    Update latchConfigAdmin.php

--- ElevenPaths/latchConfigAdmin.php
+++ ElevenPaths/latchConfigAdmin.php
@@ -4,9 +4,9 @@ require_once("APIs/latchAPI/LatchResponse.php");
 require_once("APIs/latchAPI/Error.php");
 require_once("APIs/latchAPI/LatchApp.php");
 
-$appId='HfLq4WYNKyNEMnsqDGyX';
-$secret='thy98XdBuPBxdCcrtkR8hKAFx2f77JVmscwXPh74';
-$operationLogin='CJWyXQi4RcGrcj6YMGia';
-$operationBlock='j8vyF9fwy4Xqr4rhCPxP';
+$appId='';
+$secret='';
+$operationLogin='';
+$operationBlock='';
 ?>
 
